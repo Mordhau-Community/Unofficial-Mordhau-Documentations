5179a06116de6655374ab3cc8ef27608554c21ea
JohnBrayden <john.githubx@gmail.com>
2024-12-09 17:20:51 +0300

    Fixed.

--- utils/copy-redirects.js
+++ utils/copy-redirects.js
@@ -2,6 +2,9 @@ const fs = require("fs-extra");
 const path = require("path");
 
 const source = path.resolve(__dirname, "../docs/_redirects");
-const destination = path.resolve(__dirname, "../.vitepress/dist/_redirects");
+const destination = path.resolve(
+  __dirname,
+  "../.vitepress/dist/docs/_redirects"
+);
 
 fs.copySync(source, destination, { overwrite: true });
